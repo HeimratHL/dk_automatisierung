8d046576b5c4cf0a6160662c13f631e8d2ba3355
Timo Frank <timo.frank@student.uni-luebeck.de>
2018-11-27 15:14:28 +0100

    improved menu handling

--- src/root/nested/main.py
+++ src/root/nested/main.py
@@ -10,24 +10,18 @@ title = "Heimrat Preislisten-Tool"
 class Main(tkinter.Frame):
     def __init__(self, *args, **kwargs):
         tkinter.Frame.__init__(self, *args, **kwargs)
-        p1 = Home(self)
-        p2 = Load(self)
-        p3 = Create(self)
-        
-        buttonframe = tkinter.Frame(self)
+        ## Pages ##
+        loadPage = Load(self)
+        createPage = Create(self)
+        homePage = Home(self, loadPage, createPage)
+        ## Building the page container ##
         container = tkinter.Frame(self)
-        buttonframe.pack(side="top", fill="x", expand=False)
         container.pack(side="top", fill="both", expand=True)
-        p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-        p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-        p3.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-        b1 = tkinter.Button(buttonframe, text="Page 1", command=p1.lift)
-        b2 = tkinter.Button(buttonframe, text="Page 2", command=p2.lift)
-        b3 = tkinter.Button(buttonframe, text="Page 3", command=p3.lift)
-        b1.pack(side="left")
-        b2.pack(side="left")
-        b3.pack(side="left")
-        p1.lift()
+        homePage.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        loadPage.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        createPage.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        ## Show home page by default ##
+        homePage.lift()
 
 ## Argument Parsing ##
 parser = argparse.ArgumentParser(description=title)
--- src/root/nested/pages/home.py
+++ src/root/nested/pages/home.py
@@ -1,7 +1,15 @@
 import tkinter
 
 class Home(tkinter.Frame):
-   def __init__(self, *args, **kwargs):
-       tkinter.Frame.__init__(self, *args, **kwargs)
-       label = tkinter.Label(self, text="This is page 1")
-       label.pack(side="top", fill="both", expand=True)
+    def __init__(self, master, loadPage, createPage):
+        tkinter.Frame.__init__(self, master)
+        #tkinter.Frame.configure(self, background="black")
+        tkinter.Frame.rowconfigure(self, 0, weight=1)
+        tkinter.Frame.rowconfigure(self, 1, weight=1)
+        tkinter.Frame.rowconfigure(self, 2, weight=1)
+        tkinter.Frame.rowconfigure(self, 3, weight=1)
+        tkinter.Frame.columnconfigure(self, 0, weight=1)
+        tkinter.Label(self, text="Startseite").grid(row=0)
+        tkinter.Button(self, text="Load Preset", command=lambda:loadPage.lift()).grid(row=1)
+        tkinter.Button(self, text="Create Preset", command=lambda:createPage.lift()).grid(row=2)
+        tkinter.Label(self, text="active: ...............TODO..............").grid(row=3)
