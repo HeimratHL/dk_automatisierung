aefa8bc8c0c2cd2b2b841ef45746e9217e4b2965
Timo Frank <timo.frank@student.uni-luebeck.de>
2018-11-28 02:19:57 +0100

    predefined templates now show up at loading page

--- src/root/nested/pages/load.py
+++ src/root/nested/pages/load.py
@@ -1,5 +1,9 @@
 import tkinter
+import glob
 from pages.page import Page
+
+presetFiles = glob.glob(".\presets\*.html")
+scrollCounter = 0
 
 class Load(Page):
     def __init__(self, *args, **kwargs):
@@ -13,9 +17,26 @@ class Load(Page):
         tkinter.Frame.columnconfigure(self, 1, weight=1)
         tkinter.Label(self, text="Load Preset").grid(row=0, column=1, sticky=tkinter.NE, padx=10, pady=10)
         tkinter.Button(self, text="Back", command=lambda:self.moveToPage("homePage")).grid(row=0, column=0, sticky=tkinter.NW, padx=10, pady=10)
-        tkinter.Button(self, text="Up", command=lambda:print("up")).grid(row=1, column=0)
-        tkinter.Button(self, text="Down", command=lambda:print("down")).grid(row=4, column=0)
-        tkinter.Button(self, text="Preset1", command=lambda:print("fake")).grid(row=1, column=1, sticky=tkinter.W)
-        tkinter.Button(self, text="Preset2", command=lambda:print("fake")).grid(row=2, column=1, sticky=tkinter.W)
-        tkinter.Button(self, text="Preset3", command=lambda:print("fake")).grid(row=3, column=1, sticky=tkinter.W)
-        tkinter.Button(self, text="Preset3", command=lambda:print("fake")).grid(row=4, column=1, sticky=tkinter.W)
+        tkinter.Button(self, text="Up", command=lambda:self.scrollUp()).grid(row=1, column=0)
+        tkinter.Button(self, text="Down", command=lambda:self.scrollDown()).grid(row=4, column=0)
+        tkinter.Button(self, text="empty").grid(row=1, column=1, sticky=tkinter.W)
+        tkinter.Button(self, text="empty").grid(row=2, column=1, sticky=tkinter.W)
+        tkinter.Button(self, text="empty").grid(row=3, column=1, sticky=tkinter.W)
+        tkinter.Button(self, text="empty").grid(row=4, column=1, sticky=tkinter.W)
+        self.loadPresets()
+    
+    def scrollUp(self):
+        global scrollCounter
+        if scrollCounter > 0:
+            scrollCounter = scrollCounter - 1
+            self.loadPresets()
+
+    def scrollDown(self):
+        global scrollCounter
+        if scrollCounter + 4 < len(presetFiles):
+            scrollCounter = scrollCounter + 1
+            self.loadPresets()
+
+    def loadPresets(self):
+        for num, name in enumerate(presetFiles[scrollCounter:scrollCounter + 4]):
+            tkinter.Button(self, text="{}: {}".format(num, name), command=lambda:print("test")).grid(row=num + 1, column=1, sticky=tkinter.W)
